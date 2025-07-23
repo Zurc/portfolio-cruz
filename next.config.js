682b270cc8ad8072dba34386745df237e8400cbe
Zurc <cruzjurado@gmail.com>
2025-07-23 15:36:50 +0100

    rm experimental on next config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,19 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: isProd ? "/portfolio-cruz" : "",
+  assetPrefix: isProd ? "/portfolio-cruz/" : "",
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
-  assetPrefix: isProd ? "/portfolio-cruz/" : "",
-  basePath: isProd ? "/portfolio-cruz" : "",
-  output: "export",
   trailingSlash: true, // required for static export on GitHub Pages
-  experimental: {},
 };
 
 module.exports = nextConfig;
